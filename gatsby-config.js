2e44452fbec12bb8dd7d74ec8ea272009c845be6
Dusty Garon <hello@dustygaron.com>
2020-03-07 20:02:06 -0500

    Shopify connect token

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+require('dotenv').config()
 
 module.exports = {
   plugins: [
@@ -14,8 +14,8 @@ module.exports = {
     {
       resolve: 'gatsby-source-shopify',
       options: {
-        shopName: 'Dev Life',
-        // get this: https://netlify.myshopify.com/admin/apps/private
+        shopName: 'dev-life-swag-store',
+        // get this: https://dev-life.myshopify.com/admin/apps/private
         accessToken: process.env.SHOPIFY_STOREFRONT_API_TOKEN,
         apiVersion: '2020-01',
         includeCollections: ['shop']
